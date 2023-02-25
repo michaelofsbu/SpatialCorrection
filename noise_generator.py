776de4b37cff6c8d665004aaa62d79ec5fbed334
michaelofsbu <jiachen.yao@stonybrook.edu>
2023-02-25 10:45:15 -0800

    Upate noise generator

--- noise_generator.py
+++ noise_generator.py
@@ -1,13 +1,10 @@
-import os 
+import os, sys
+import argparse
 from PIL import Image
-import torch
 import random
 import numpy as np
-from scipy import signal
-from copy import copy
 import shutil
 import cv2
-import matplotlib.pyplot as plt
 import SimpleITK as sitk
 from scipy.ndimage import gaussian_filter
 from scipy.ndimage.morphology import distance_transform_edt as distrans
@@ -15,11 +12,11 @@ from scipy.ndimage.morphology import distance_transform_edt as distrans
 def get_args(args_to_parse):
     parser = argparse.ArgumentParser(description='Unet')
     # Paths
-    parser.add_argument('--gts_root', type=str, default='../Datasets/JSRT/heart_gts_train/'
+    parser.add_argument('--gts_root', type=str, default='./Datasets/JSRT/heart_gts_train/',
                                     help='Directory root of groundtruth masks.')
-    parser.add_argument('--save_root', type=str, default='../Datasets/JSRT/heart_250_0.8_0.05_0.2/'
+    parser.add_argument('--save_root', type=str, default='./Datasets/JSRT/heart_250_0.8_0.05_0.2/',
                                     help='Directory root for saving generated noisy masks.')
-    parser.add_argument('--3D', type=bool, default=False,
+    parser.add_argument('--is3D', type=bool, default=False,
                                     help='Whether the dataset is 3D valume.')
     parser.add_argument('--noisetype', type=str, default='Markov', choices=['Markov', 'DE'],
                                     help='Noise type. Markov: the proposed Markov random noise. DE: dilation and erosion noise.')
@@ -37,34 +34,53 @@ def get_args(args_to_parse):
     # Parameter for Dilation and Erosion noise
     parser.add_argument('--range', type=list, default=[9, 11],
                                     help='The range of noise level.')
+    args = parser.parse_args(args_to_parse)
     return args
 
 def generate(args):
+    '''
+        The main function for generating deferent types of noise
+    '''
     if not os.path.isdir(args.save_root):
         os.makedirs(args.save_root)
     gts = sorted([f for f in os.listdir(args.gts_root) if not f.startswith('.')])
     for i in range(len(gts)):
         file = os.path.join(args.gts_root, gts[i])
         save = os.path.join(args.save_root, gts[i])
+        # Generate our Markov noise
         if args.noisetype == 'Markov':
-            if not args.3D:
+            if not args.is3D:
                 _MarkovNoise_random(file, save, args.T, args.theta1, args.theta2, args.theta3)
             else:
                 _MarkovNoise_random_3D(file, save, args.T, args.theta1, args.theta2, args.theta3)
+        # Generate random dilation and erosion noise
         elif args.noisetype == 'DE':
-            if not args.3D:
+            if not args.is3D:
                 _DilateErodeNoise(file, save, args.range)
             else:
                 _DilateErodeNoise_3D(file, save, args.range)
+        break
 
 def _gradient(img):
+    '''
+        Calculate gradient of given image, for visualization only
+    '''
     kernely = np.array([[1,1,1],[1,-8,1],[1,1,1]])
-    #kernelx = np.array([[1,0,-1],[1,0,-1],[1,0,-1]])
-    #edges_x = cv2.filter2D(img,-1,kernelx)
     edges_xy = cv2.filter2D(img,-1,kernely)
     return edges_xy
 
+def _dice(gt, noise):
+    '''
+        Calculate dice score for true mask and noise mask, for evaluating noise level
+    '''
+    eps = 1e-5
+    inter = (gt*noise).sum().astype(np.float32)
+    return 2*inter/(gt.sum()+noise.sum() + eps).astype(np.float32)
+
 def _DilateErodeNoise(gt_path, save_path, noiseRange):
+    '''
+        Random dilation an erosion noise, 2D version
+    '''
     ntype = np.random.rand()
     gt = np.asarray(Image.open(gt_path))/255
     if ntype > 0.5:
@@ -76,6 +92,9 @@ def _DilateErodeNoise(gt_path, save_path, noiseRange):
     gt_noise.save(save_path)
 
 def _DilateErodeNoise_3D(gt_path, save_path, noiseRange):
+    '''
+        Random dilation an erosion noise, 3D version
+    '''
     ntype = np.random.rand()
     gt = sitk.GetArrayFromImage(sitk.ReadImage(gt_path)).astype(np.float32)
     gt[gt>0] = 1
@@ -89,6 +108,9 @@ def _DilateErodeNoise_3D(gt_path, save_path, noiseRange):
     sitk.WriteImage(noise, save_path)
 
 def _dilateORerode(input, ntype, nRange):
+    '''
+        Dilate or erode given mask, depending on 'ntype'
+    '''
     tmp = np.zeros(input.shape)
     nlevel = nRange[0] + (nRange[1] - nRange[0]) * np.random.rand()
     if ntype == 1:
@@ -97,24 +119,15 @@ def _dilateORerode(input, ntype, nRange):
         tmp = 1 - (distrans(input) < nlevel).astype(np.float32)
     return tmp
 
-def _mix_noise(gtpath, gtpath1, savepath, rate):
-    if not os.path.isdir(savepath):
-        os.makedirs(savepath)
-    gts = sorted([f for f in os.listdir(gtpath) if not f.startswith('.')])
-    l = int(len(gts)*rate)
-    np.random.shuffle(gts)
-    for i in range(0,l):
-        shutil.copy(gtpath + gts[i], savepath + gts[i])
-    for i in range(l, len(gts)):
-        shutil.copy(gtpath1 + gts[i], savepath + gts[i])
-
 def _MarkovNoise_random(gtpath, savepath, T, theta1, theta2, theta3):
+    '''
+        Generate the proposed Markov noise, 2D version
+    '''
     gt = np.array(Image.open(gtpath))
     noise = gt.copy()/255
     ps = np.random.rand(T)
     for t in range(T):
         if ps[t] < theta1:
-            #edge = ((distrans(1-noise)<1.5)&(distrans(1-noise)>0)).nonzero()
             edge = (distrans(1-noise)==1).nonzero()
             edge = list(zip(edge[0], edge[1]))
             prob = np.random.rand(len(edge))
@@ -122,9 +135,7 @@ def _MarkovNoise_random(gtpath, savepath, T, theta1, theta2, theta3):
                 e = edge[i]
                 if prob[i] < theta2: #flip
                     noise[e[0], e[1]]  = 1 - noise[e[0], e[1]]
-            #print('erode')
         else:
-            #edge = ((distrans(noise)<1.5)&(distrans(noise)>0)).nonzero()
             edge = (distrans(noise)==1).nonzero()
             edge = list(zip(edge[0], edge[1]))
             prob = np.random.rand(len(edge))
@@ -132,36 +143,25 @@ def _MarkovNoise_random(gtpath, savepath, T, theta1, theta2, theta3):
                 e = edge[i]
                 if prob[i] < theta2: #flip
                     noise[e[0], e[1]]  = 1 - noise[e[0], e[1]]
-            # if t%80 == 0:
-            #     noise = (noise*255).astype(np.uint8)
-            #     noise = cv2.GaussianBlur(noise, (5, 5), 2, 2)
-            #     noise = np.array((noise/255)>0.5)
-            #print('erode')
     mask = np.random.rand(noise.shape[0], noise.shape[1])
     noise = np.where(mask>theta3, noise, 1-noise)
-    #noise = Image.fromarray((noise*255).astype(np.uint8))
-    #noise.save(savepath)
     noise = (noise*255).astype(np.uint8)
     noise = cv2.GaussianBlur(noise, (5, 5), 2, 2)
     noise = np.array(noise/255>0.5)
-    print(_dice(gt/255, noise))
+    print('Dice {:.2f}'.format(_dice(gt/255, noise)))
     noise = Image.fromarray((noise*255).astype(np.uint8))
     noise.save(savepath)
 
-def _dice(gt, pred):
-    eps = 1e-5
-    #pred = (pred > 0.5).astype(np.float)
-    inter = (gt*pred).sum().astype(np.float32)
-    return 2*inter/(gt.sum()+pred.sum() + eps).astype(np.float32)
-
 def _MarkovNoise_random_3D(gtpath, savepath, T, theta1, theta2, theta3):
+    '''
+        Generate the proposed Markov noise, 2D version
+    '''
     gt = sitk.GetArrayFromImage(sitk.ReadImage(gtpath)).astype(np.float32)
     gt[gt>0] = 1
     noise = gt.copy()
     ps = np.random.rand(T)
     for t in range(T):
         if ps[t] < theta1:
-            #edge = ((distrans(1-noise)<1.5)&(distrans(1-noise)>0)).nonzero()
             edge = (distrans(1-noise)==1).nonzero()
             edge = list(zip(edge[0], edge[1], edge[2]))
             prob = np.random.rand(len(edge))
@@ -169,9 +169,7 @@ def _MarkovNoise_random_3D(gtpath, savepath, T, theta1, theta2, theta3):
                 e = edge[i]
                 if prob[i] < theta2: #flip
                     noise[e[0], e[1], e[2]]  = 1 - noise[e[0], e[1], e[2]]
-            #print('erode')
         else:
-            #edge = ((distrans(noise)<1.5)&(distrans(noise)>0)).nonzero()
             edge = (distrans(noise)==1).nonzero()
             edge = list(zip(edge[0], edge[1], edge[2]))
             prob = np.random.rand(len(edge))
@@ -179,31 +177,14 @@ def _MarkovNoise_random_3D(gtpath, savepath, T, theta1, theta2, theta3):
                 e = edge[i]
                 if prob[i] < theta2: #flip
                     noise[e[0], e[1], e[2]]  = 1 - noise[e[0], e[1], e[2]]
-            # if t%80 == 0:
-            #     noise = (noise*255).astype(np.uint8)
-            #     noise = cv2.GaussianBlur(noise, (5, 5), 2, 2)
-            #     noise = np.array((noise/255)>0.5)
-            #print('erode')
     mask = np.random.rand(noise.shape[0], noise.shape[1], noise.shape[2])
     noise = np.where(mask>theta3, noise, 1-noise)
-    #noise = Image.fromarray((noise*255).astype(np.uint8))
-    #noise.save(savepath)
     noise = (noise*255).astype(np.uint8)
     noise = gaussian_filter(noise, 1, mode='nearest')
-    # for i in range(len(noise)):
-    #     noise[i] = cv2.GaussianBlur(noise[i], (5, 5), 2, 2)
     noise = np.array(noise/255>0.5)
-    print(_dice(gt, noise))
+    print('Dice {:.2f}'.format(_dice(gt, noise)))
     noise = sitk.GetImageFromArray((noise*255).astype(np.uint8))
     sitk.WriteImage(noise, savepath)
-    '''
-    edge = gradient(gt)
-    plt.figure()
-    plt.imshow(noise, cmap='gray')
-    plt.scatter(edge.nonzero()[1], edge.nonzero()[0], s=0.5, c='red')
-    plt.show()
-    #plt.savefig('./markovnoise.png')
-    '''
 
 if __name__ == '__main__':
     args = get_args(sys.argv[1:])
